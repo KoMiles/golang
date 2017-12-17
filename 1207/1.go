9709cbec61c029e5e7995575b24fdd7e41d35c4a
kongming <komiles@163.com>
2017-12-17 23:29:49 +0800

    first commit

---
+++ 1207/1.go
@@ -0,0 +1,21 @@
+package main
+
+import std "fmt"
+
+const PI = 3.14
+const (
+	ADVERT_ONLINE  = 1
+	ADVERT_OFFLINE = 2
+)
+
+var name = "gopher"
+
+type newType = int
+
+type gopher struct{}
+
+type golang interface{}
+
+func main() {
+	std.Println("Hello world")
+}
