fd3b3595d8189798ac91d5a72daa7cedd274054d
kongming <komiles@163.com>
2017-12-18 21:34:18 +0800

    *添加go文件

---
+++ 1218/004.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 42
+	var f float64 = float64(i)
+	var u uint = uint(f)
+	fmt.Printf("%v %v %v ",i,f,u)
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # golang
 
 ### golang学习笔记
+
+- [12-18] https://tour.go-zh.org/basics/14
